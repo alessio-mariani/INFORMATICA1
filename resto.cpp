9f75862a4ddc519f616af032291e4f98e7738829
alessio-mariani <113691724+alessio-mariani@users.noreply.github.com>
2022-10-14 12:47:39 +0200

    Add files via upload

---
+++ resto.cpp
@@ -0,0 +1,55 @@
+/*
+file: restoDivisione.c
+autore: alessio mariani
+classe: 3°H
+data: 05/10/2022
+Restituisci il resto della divisione tra due interi 
+Input: n1, n2
+Elaborazione: calcolo resto
+Output: resto
+*/
+
+#include <stdio.h>
+
+int main() {
+  int n1, n2; //input
+  int resto;  //output
+  
+  printf("Inserisci il primo numero: ");  //lettura primo numero
+  scanf("%d", &n1);
+  
+  printf("Inserisci il secondo numero: ");  //lettura secondo numero
+  scanf("%d", &n2);
+  
+  resto = n1 % n2;  //calcolo resto divisione
+  
+  printf("Resto: %d", resto); //scrittura risultato
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
